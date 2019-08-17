3d4cf0cca1685991788e86c55b30c21ad5fb6c33
Pawel Wolski <pewsonik@gmail.com>
2019-08-17 14:27:47 +0200

    first init

---
+++ src/main/java/pl/sdacademy/tasq/TasqApplication.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.tasq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sdacademy/tasq/TasqApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.sdacademy.tasq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TasqApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
