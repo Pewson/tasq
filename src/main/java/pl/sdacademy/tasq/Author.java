49bc84bd313018117ea66cd04b447ed9e353e52d
KaterinaI <ekaterinayaroshovets@gmail.com>
2019-08-18 15:06:12 +0200

    [Add]classes: User i Author (2)

--- src/main/java/pl/sdacademy/tasq/Author.java
+++ src/main/java/pl/sdacademy/tasq/Author.java
@@ -1,4 +1,65 @@
 package pl.sdacademy.tasq;
 
+/*6. Autor
+- id (uuid) (pk)
+- imię
+- nazwisko
+- lista testów (fk)
+*/
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
 public class Author {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private String id;
+    private  String name;
+    private  String lastName;
+
+
+    @OneToMany(fetch = FetchType.EAGER)
+    private List<Test> tests;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Test> getTests() {
+        return tests;
+    }
+
+    public void setTests(List<Test> tests) {
+        this.tests = tests;
+    }
 }
--- src/main/java/pl/sdacademy/tasq/User.java
+++ src/main/java/pl/sdacademy/tasq/User.java
@@ -1,4 +1,65 @@
 package pl.sdacademy.tasq;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+/*5. Użytkownik
+- id (uuid) (pk)
+- imię
+- nazwisko
+- lista testów (fk)
+*/
+
 public class User {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private String id;
+
+    private  String name;
+    private  String lastName;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    private List<Test> tests;
+
+    public List<Test> getTests() {
+        return tests;
+    }
+
+    public void setTests(List<Test> tests) {
+        this.tests = tests;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
