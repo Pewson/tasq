ff6a5e76e7dd9506dcc2b158d38782ba2066e2cd
Pawel Wolski <pewsonik@gmail.com>
2019-08-18 15:17:58 +0200

    [ADD] Classes: Answer, UserAnswer

--- src/main/java/pl/sdacademy/tasq/TasqApplication.java
+++ src/main/java/pl/sdacademy/tasq/TasqApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TasqApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TasqApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TasqApplication.class, args);
+    }
 
 }
