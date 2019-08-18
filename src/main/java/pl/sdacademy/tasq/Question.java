276e8dc8a9900e7f6817c8935228c12b93b28f0a
zaxonspox <zaxonspox@gmail.com>
2019-08-18 15:03:57 +0200

    [add] classes: Test, Question (2)

--- src/main/java/pl/sdacademy/tasq/Question.java
+++ src/main/java/pl/sdacademy/tasq/Question.java
@@ -1,4 +1,73 @@
 package pl.sdacademy.tasq;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name="Question")
+@Table(name="question")
 public class Question {
+
+    @Id
+    @GeneratedValue(generator = “UUID”)
+    @GenericGenerator(
+            name = “UUID”,
+            strategy = “org.hibernate.id.UUIDGenerator”,
+    )
+    private Integer id;
+
+    @ManyToOne(fetch=FetchType.EAGER)
+    @JoinColumn(name = "id")
+    private Test test;
+
+    @Column(name="text")
+    private String text;
+
+    @Column(name="points")
+    private Integer points;
+
+    @OneToMany(fetch=FetchType.EAGER)
+    private List<Answer> listOfAnswers;
+    //private Answer answer;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Test getTest() {
+        return test;
+    }
+
+    public void setTest(Test test) {
+        this.test = test;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+
+    public List<Answer> getListOfAnswers() {
+        return listOfAnswers;
+    }
+
+    public void setListOfAnswers(List<Answer> listOfAnswers) {
+        this.listOfAnswers = listOfAnswers;
+    }
 }
--- src/main/java/pl/sdacademy/tasq/Test.java
+++ src/main/java/pl/sdacademy/tasq/Test.java
@@ -1,4 +1,81 @@
 package pl.sdacademy.tasq;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name="Test")
+@Table(name="test")
 public class Test {
+
+    @Id
+    @GeneratedValue(generator = “UUID”)
+    @GenericGenerator(
+            name = “UUID”,
+            strategy = “org.hibernate.id.UUIDGenerator”,
+    )
+    private String id;
+    @Column(name="title")
+    private String title;
+
+    @Column(name="idAuth")
+    private Integer idAuth;
+
+    @ManyToOne(fetch=FetchType.EAGER)
+    private Author author;
+
+    @OneToMany(fetch=FetchType.EAGER)
+    private List<Question> questions;
+
+    @Column(name="type")
+    private DocType type;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getIdAuth() {
+        return idAuth;
+    }
+
+    public void setIdAuth(Integer idAuth) {
+        this.idAuth = idAuth;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+
+    public DocType getType() {
+        return type;
+    }
+
+    public void setType(DocType type) {
+        this.type = type;
+    }
 }
